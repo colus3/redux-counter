947178cd4a9a9e35ac7f375c89eadb2e4ad71847
Donghwan Lee <colus4@gmail.com>
2018-09-17 18:33:04 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+// 리덕스 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
+// 스토어 생성
+const store = createStore(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
